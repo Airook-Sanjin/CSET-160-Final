0abbc50b7ca92b4f4fc367c01fa272efc7401a1c
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-03-26 08:19:24 -0400

    Insert Base HTML

--- Base.py
+++ Base.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
+# examConn = 'SQL STRING'
+# engine = create_engine(connectionString, echo = True)
+# conn = engine.connect()
+
+
+@app.route("/")
+def Base():
+        return render_template("Base.html")
 if __name__ == '__main__':
         app.run()
     
