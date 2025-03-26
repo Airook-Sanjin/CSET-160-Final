4a5d0fe24ee9d60d457c88588bf767b588eb5d23
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-03-26 12:27:53 -0400

    Added Teacher Code

--- Base.py
+++ Base.py
@@ -37,8 +37,11 @@ def createAccount():
     try:
         RadioValue= request.form["Teach-Stud"]
         if RadioValue == "1":
-            prevID = conn.execute(text("select Sid from student order by Sid desc Limit 1;")).one()
-            newID = int(prevID[0])+1
+            prevID = conn.execute(text("select Sid from student order by Sid desc Limit 1;")).fetchone()
+            if not prevID:
+                newID = 1
+            else:
+                newID = int(prevID[0])+1
             
             conn.execute(text("insert into student(Sid, first_name, last_name, password) values (:Sid, :first_name, :last_name, :password)"), {"Sid": newID, "first_name":request.form["first_name"], "last_name":request.form["last_name"],"password":request.form["password"]}
                          )
@@ -47,11 +50,19 @@ def createAccount():
             for row in result:
                 print(row)
         else:
-            prevID = conn.execute(text("select Tid from teacher order by Tid desc Limit 1;"))
-            newID = int(prevID[0])+1
-            conn.execute(text("insert into teacher values(:Tid, :first_name, :last_name, :password)"), {"Tid": newID, "first_name":request.form["first_name"], "last_name":request.form["last_name"],"password":request.form["password"]})
-            # print(TeacherTbl)
-        # to add to the database
+            prevID = conn.execute(text("select Tid from teacher order by Tid desc Limit 1;")).fetchone()
+            if not prevID:
+                newID = 1
+            else:
+                newID = int(prevID[0])+1
+                
+            conn.execute(text("insert into teacher(Tid, first_name, last_name, password) values(:Tid, :first_name, :last_name, :password)"), {"Tid": newID, "first_name":request.form["first_name"], "last_name":request.form["last_name"],"password":request.form["password"]})
+            conn.commit()
+            result = conn.execute(text('select * from teacher')).fetchall()
+            
+            for row in result:
+                print(row)
+            
         return render_template("Register.html", error = None, success = "Successfull")
     except Exception as e:
         print(f"Error: {e}")
